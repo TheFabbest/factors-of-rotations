e8f6eb7c221fe252c0fefcaca04e58a06c1daad9
Fab <fabrizioapuzzo2003@gmail.com>
2025-06-07 19:22:35 +0200

    Update optimalSuffixArray.hpp

--- src/optimalSuffixArray.hpp
+++ src/optimalSuffixArray.hpp
@@ -3,7 +3,8 @@
 
 #include <algorithm>
 
-// auxiliary functions
+// ----- auxiliary functions
+
 int compareSuffixes(const char * const input, const unsigned long length, const unsigned long offsetA, const unsigned long offsetB) {
     unsigned long i = 0;
     while (i < length) {
@@ -14,6 +15,35 @@ int compareSuffixes(const char * const input, const unsigned long length, const 
     }
     return 0; // equal suffixes
 }
+
+void Heapify(unsigned long *array, unsigned long n, unsigned long index) {
+    while (true) {
+        unsigned long smallest = index;
+        const unsigned long left = 2 * index + 1;
+        const unsigned long right = 2 * index + 2;
+        if (left < n && array[left] < array[smallest]) {
+            smallest = left;
+        }
+        if (right < n && array[right] < array[smallest]) {
+            smallest = right;
+        }
+        if (smallest != index) {
+            std::swap(array[index], array[smallest]);
+            index = smallest;
+        } else {
+            break; // heap property is satisfied
+        }
+    }
+}
+
+void BuildHeap(unsigned long *array, unsigned long n){
+    for (unsigned long i = n/2+1; i > 0; --i) {
+        const unsigned long index = i - 1;
+        Heapify(array, n, index);
+    }
+}
+
+// end of auxiliary functions -----
 
 // section 5.2 - step 1
 unsigned long placeIndicesOfS_Type(const char * const input, const unsigned long length, unsigned long *SA) {
@@ -67,6 +97,16 @@ void constructReducedProblem(const char * const input, const unsigned long lengt
     }
 }
 
+
+// section 5.3 - step 2
+void heapSortReducedProblem(unsigned long *SA, unsigned long length, const unsigned long nS) {
+    BuildHeap(SA + nS, length - nS);
+    for (unsigned long i = length; i > nS; --i) {
+        std::swap(SA[nS], SA[i]);
+        Heapify(SA + nS, i, 0);
+    }
+}
+
 // section 5.4
 void RestoreFromRecursion(const char * const input, const unsigned long length, unsigned long *SA, const unsigned long nS) {
     bool nextIsL = true;
@@ -84,5 +124,23 @@ void RestoreFromRecursion(const char * const input, const unsigned long length, 
     for (unsigned long i = length-nS-1; i < length; ++i) {
         SA[i] = SA[SA[i]];
     }
+}
+
+// section 5.5 - preprocessing
+void preprocess(const char * const input, unsigned long *SA, const unsigned long length, const unsigned long nS) {
+    bool nextIsL = true; // last character is always L_TYPE because of the sentinel at the end (which is S_TYPE)
+    unsigned long last_inserted_index = length-nS;
+    for (unsigned long i = 0; i < length-1; ++i) {
+        const unsigned long index = length - i - 2;
+        const bool currentIsL = input[index] > input[index+1] || (nextIsL && input[index] == input[index+1]);
+        if (currentIsL) {
+            SA[--last_inserted_index] = index;
+        }
+        nextIsL = currentIsL;
+    }
+
+    // TODO
+    //Then, we sort SA[0 ... n − 1] (the sorting key of SA[i] is T [SA[i]] i.e., the ﬁrst character of suf(SA[i]))
+    // using the mergesort, with the merging step implemented by the stable, in-place, linear time merging algorithm
 
 }
